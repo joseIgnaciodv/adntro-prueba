1fb9f6e7ddc790040f65000d1b332fb1a01a833c
joseIgnaciodv <joseignaciodelvallebustillo@gmail.com>
2024-12-03 23:36:22 +0100

    importar modulo http, y los componentes (FileSelection) y (GeneticMetrics) a la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { GeneticMetricsComponent } from './components/genetic-metrics/genetic-metrics.component';
+import { FileSelectionComponent } from './components/file-selection/file-selection.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, GeneticMetricsComponent, FileSelectionComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
